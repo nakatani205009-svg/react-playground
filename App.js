939851f28f353651b1571f39024effa4c1d8aeae
nakatani205009-svg <nakatani205009@gmail.com>
2025-08-21 11:48:44 +0900

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+      <h1>Welcome to React Playground</h1>
+      <p>This is a simple React app ready for experimentation.</p>
+    </div>
+  );
+}
+
+export default App;
